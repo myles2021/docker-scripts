bb8b22d1705a9b20d66d62fab1847acf41cde911
myles2021 <86742200+myles2021@users.noreply.github.com>
2022-02-01 14:24:21 +0000

    Create docker-compose-CLI.sh

---
+++ docker-compose-CLI.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/mrbilalshafiq/frontend-and-backend-python
+cd frontend-and-backend-python
+docker-compose up
+docker-compose up -d
+docker-compose logs
+docker-compose logs -f
+git checkout red-background
+docker-compose build
+docker-compose up -d
+curl localhost:5000
+git checkout main
+docker-compose build
+docker-compose up -d
+curl localhost:5000
+docker-compose down --rmi all
