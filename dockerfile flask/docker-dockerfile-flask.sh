3056892c6749af026accd762ea62925ba200e43f
myles2021 <86742200+myles2021@users.noreply.github.com>
2022-02-28 09:59:29 +0000

    automation of printf update

--- dockerfile flask/docker-dockerfile-flask.sh
+++ dockerfile flask/docker-dockerfile-flask.sh
@@ -1,31 +1,11 @@
-mkdir myapp
-cd myapp
+mkdir myapp && cd $_
 touch app.py
-code app.py 
-# @app.route('/home')
-# @app.route('/')
-# def home():
-#     return "Hello world!"
 
-# if __name__ == '__main__':
-#     app.run(port=5000, host='0.0.0.0', debug=True)
+printf "@app.route('/home')\n@app.route('/')\ndef home():\n    return \"Hello world!\"\n\nif __name__ == '__main__':\n    app.run(port=5000, host='0.0.0.0', debug=True)"
 
 touch Dockerfile
-code Dockerfile 
-# # Python base image.
-# FROM python:3.7
-# # Create and set the work directory inside the image named 'app'
-# WORKDIR /app
-# # Execute a pip install command using the list 'requirements.txt'
-# RUN pip install Flask
-# # Copy the app file into the image working directory
-# COPY app.py .
-# # State the listening port for the container. 
-# EXPOSE 5000
-# # Run 'python app.py' on container start-up. This is the main process.
-# ENTRYPOINT ["python", "app.py"]
 
-
+printf "# Python base image.\nFROM python:3.7\n# Create and set the work directory inside the image named 'app'\nWORKDIR /app\n# Execute a pip install command using the list 'requirements.txt'\nRUN pip install Flask\n# Copy the app file into the image working directory\nCOPY app.py .\n# State the listening port for the container. \nEXPOSE 5000\n# Run 'python app.py' on container start-up. This is the main process.\nENTRYPOINT [\"python\", \"app.py\"]"
 
 docker build -t myapp .
 docker run -d -p 5000:5000 --name myapp myapp
