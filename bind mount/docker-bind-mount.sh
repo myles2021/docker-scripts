c0a7cd07c94de43232300d536fd25ed947b2a3b1
myles2021 <86742200+myles2021@users.noreply.github.com>
2022-03-03 14:51:56 +0000

    Update docker-bind-mount.sh

--- bind mount/docker-bind-mount.sh
+++ bind mount/docker-bind-mount.sh
@@ -1,7 +1,7 @@
 mkdir bind_mounts_exercise
 touch bind_mounts_exercise/nginx.conf
 cd bind_mounts_exercise/
-code nginx.conf 
+# code nginx.conf 
 # events {}
 # http {
 #    server {
@@ -12,7 +12,8 @@ code nginx.conf
 #    }
 # }
 
-vi nginx.conf
+printf '\nevents {}\nhttp {\n   server {\n       listen 80;\n       location / {\n           return 200 "NGINX";\n       }\n   }\n}\n' >> nginx.conf
+
 docker run -d -p 80:80 --name nginx --mount type=bind,source=$(pwd)/nginx.conf,target=/etc/nginx/nginx.conf nginx
 curl localhost
 docker stop nginx
