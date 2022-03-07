5082e88197e1a7d197c9fb50ca8707eb63cb5d29
myles2021 <86742200+myles2021@users.noreply.github.com>
2022-03-07 11:43:40 +0000

    Update qa_cc_tutorial.sh

--- dockerfile nginx/qa_cc_tutorial.sh
+++ dockerfile nginx/qa_cc_tutorial.sh
@@ -8,3 +8,10 @@ docker run -d --rm -p 8888:80 --name web nginx:1.19.0
 
 docker run -d --name pine challenge
 
+
+touch Dockerfile
+
+printf 'FROM busybox:1.31.1\nCOPY . .\nENV FLASK_RUN_HOST=0.0.0.0\nEXPOSE 5000\nCMD ["nc", "-lv", "-p", "5000;"]' >> Dockerfile
+docker build -t myapp:0.0.1 .
+
+docker alpine:3.12.0 alpine:challenge
